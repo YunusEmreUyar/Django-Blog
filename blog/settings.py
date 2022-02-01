c8554c450d99e562446927e2ab10350ba7637c35
Yunus Emre Uyar <yunusemreuyarr@outlook.com>
2022-02-01 14:29:17 +0300

    Update settings DEBUG val.

--- blog/settings.py
+++ blog/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4+itcgq)%2+crlvkm1v14%u)#g7^n0z$az_-z&x-uqv6+8)v+s'
+SECRET_KEY = '4+itcgq)%2+crlvkm1v1490^+^+u)#g7^n0z$az_-z&x-uqv6+8)v+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ["DEBUG_MODE"] == 'True')
+DEBUG = os.environ["DEBUG_MODE"] == 'True'
 
 
 ALLOWED_HOSTS = ["artandmovieisnotgonnabethename.herokuapp.com", "127.0.0.1"]
