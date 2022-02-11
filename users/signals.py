0edf1de0a29404f2206446e2873ac26555e8fb08
Yunus Emre Uyar <yunusemreuyarr@outlook.com>
2022-02-11 16:43:56 +0300

    Fix SMTP Error in register api view

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 from django.dispatch import receiver
 from .models import Profile
 from django.core.mail import EmailMessage
@@ -11,18 +12,21 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
+        print("----------------------------------------------")
+        model_dict = model_to_dict(instance)
         current_site = "http://artandmovieisnotgonnabethename.herokuapp.com"
         mail_subject = 'Activate your account.'
         message = render_to_string('acc_active_email.html', {
-            'user': sender,
+            'user': instance,
             'domain': current_site,
-            'uid':urlsafe_base64_encode(force_bytes(sender.pk)),
-            'token':account_activation_token.make_token(sender),
+            'uid':urlsafe_base64_encode(force_bytes(instance.pk)),
+            'token':account_activation_token.make_token(instance),
         })
-        to_email = sender.email
+        to_email = model_dict["email"]
         email = EmailMessage(mail_subject, message, to=[to_email])
         email.send()
         Profile.objects.create(user=instance)
+        
 
 
 @receiver(post_save, sender=User)
